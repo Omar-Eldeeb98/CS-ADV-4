7bfe88c487db0bc8246d89dfa730fbce1cefdade
Omar-Eldeeb98 <oeldeeb111@gmail.com>
2024-08-11 13:13:06 +0300

    session demo

---
+++ CS-ADV-4/FiFa/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_ADV_4.FiFa
+{
+    internal class Player
+    {
+        public string name { get; set; }
+        public string teamName { get; set; }
+
+        // callback function
+        public void run(Ball ball)
+        {
+            Console.WriteLine($"Player: {name} is running... , ball:{ball }");
+
+        }
+    }
+}
---
+++ CS-ADV-4/Youtube/Channel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_ADV_4.Youtube
+{
+    internal class Channel
+    {
+
+        public string name { get; set; }
+        public List<Video> videos { get; set; } = new List<Video>();
+
+        public void addVideo(Video video)
+        {
+
+            videos.Add(video);
+
+            // notify the subscriber
+            videadded?.Invoke(video, this);
+
+        }
+
+
+        public event Action<Video,Channel>? videadded;
+
+
+        public override string ToString()
+        {
+            return $"chanel name : {name}";
+        }
+    }
+}
